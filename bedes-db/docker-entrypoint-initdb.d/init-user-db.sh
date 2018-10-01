9d0f3b519fa028527a75be12d85643dac1565941
Michael Spears <mspears@lbl.gov>
2018-10-01 09:58:38 -0700

    Initial commit - Docker database setup.

---
+++ bedes-db/docker-entrypoint-initdb.d/init-user-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER ${DB_ADMIN_USER} with encrypted password '${DB_ADMIN_PASSWORD}';
+    CREATE USER ${DB_APP_USER} with encrypted password '${DB_APP_PASSWORD}';
+    CREATE DATABASE ${DB_NAME};
+    GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_ADMIN_USER};
+    ALTER DATABASE ${DB_NAME} OWNER TO ${DB_ADMIN_USER};
+EOSQL
