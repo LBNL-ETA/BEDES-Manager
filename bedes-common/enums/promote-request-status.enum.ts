b2b474fe6cd13cb7b5815719f755133e5e17bf46
mspears <mspears@lbl.gov>
2019-03-15 15:37:39 -0700

    Updated database to handle scoped composite terms, created corresponding enums and lists.

---
+++ bedes-common/enums/promote-request-status.enum.ts
@@ -0,0 +1,9 @@
+
+/**
+ * The different states a request for promotion from public -> private can be in
+ */
+export enum PromoteRequestStatus {
+    Pending=1,
+    Approved,
+    Rejected
+}
--- scripts/db/db-drop.sql
+++ scripts/db/db-drop.sql
@@ -1,3 +1,4 @@
+drop table if exists public.composite_term_promote_request;
 drop table if exists public.composite_term_maps;
 drop table if exists public.atomic_term_maps;
 drop table if exists public.atomic_term_list_option_maps;
@@ -12,8 +13,8 @@ drop table if exists public.definition_source;
 drop table if exists public.term_category;
 drop table if exists public.sector;
 
-drop table if exists public.mapping_application_requests;
-drop table if exists public.application_request_status;
+drop table if exists public.application_promote_request;
+drop table if exists public.promote_request_status;
 drop table if exists public.mapping_application_roles;
 drop table if exists public.app_enumerated_values;
 drop table if exists public.app_term_additional_data;
@@ -26,6 +27,7 @@ drop table if exists public.data_type;
 drop table if exists public.term_type;
 drop table if exists public.application_scope;
 drop table if exists public.application_role_type;
+drop table if exists public.scope;
 
 drop table if exists auth.registration_code;
 drop table if exists auth.user_group_membership;
