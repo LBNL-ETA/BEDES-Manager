3be5a0d471f4d31d85ff2cb2537973cb12ee8a72
mspears <mspears@lbl.gov>
2019-09-16 12:14:42 -0700

    made enter trigger a button click for pw resets

--- bedes-common/models/bedes-data-type/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bedes_data_type_1 = require("./bedes-data-type");
-exports.BedesDataType = bedes_data_type_1.BedesDataType;
