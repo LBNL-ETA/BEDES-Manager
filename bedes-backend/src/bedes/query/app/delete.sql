17c3ef71059ea0d51c8be0a12f7898cfdc84d1e3
Pranav Gupta <phgupta@ucdavis.edu>
2020-06-02 15:18:33 -0700

    deletes app even if it contains terms in it

--- bedes-backend/src/bedes/query/app/delete.sql
+++ bedes-backend/src/bedes/query/app/delete.sql
@@ -1,5 +1,13 @@
-delete from
-    public.mapping_application
-where
-    id = ${_id}
-;
+/* TODO: Update this to a concise single query */
+with terms as (
+    select * from public.app_term where app_id = ${_id}
+)
+delete from public.atomic_term_list_option_maps atlom using terms as t where t.id = atlom.app_term_id;
+
+with terms as (
+    select * from public.app_term where app_id = ${_id}
+)
+delete from public.app_term_list_option atlo using terms as t where t.id = atlo.app_term_id;
+
+delete from public.app_term where app_id = ${_id};
+delete from public.mapping_application where id = ${_id};
