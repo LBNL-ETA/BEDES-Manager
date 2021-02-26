e8b067a5736e5823cd9262a2f8aaca5dbb3a3189
Kevin Kaland <kevin@wizone.solutions>
2021-02-26 22:50:57 +0100

    Fix entrypoint for Heroku.

--- build/lib/backend-entrypoint.sh
+++ build/lib/backend-entrypoint.sh
@@ -4,10 +4,20 @@ set -e
 cmd="npm run run-compose"
 
 echo "Waiting for Postgres..."
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h $DB_HOST -U postgres -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 2
-done
 
->&2 echo "Postgres is up, running node"
+# Heroku mode
+if test -n ${DATABASE_URL}; then
+  until psql "$DATABASE_URL" -c '\q'; do
+    echo >&2 "Postgres is unavailable - sleeping"
+    sleep 2
+  done
+# Docker Compose mode
+else
+  until PGPASSWORD=$POSTGRES_PASSWORD psql -h $DB_HOST -U postgres -c '\q'; do
+    echo >&2 "Postgres is unavailable - sleeping"
+    sleep 2
+  done
+fi
+
+echo >&2 "Postgres is up, running node"
 exec $cmd
