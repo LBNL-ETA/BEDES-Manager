1aec7f9640c32a34fa7fcb98acd722335ce3ce0f
Kevin Kaland <kevin@wizone.solutions>
2021-02-26 22:58:02 +0100

    Check if DATABASE_URL detected.

--- build/lib/backend-entrypoint.sh
+++ build/lib/backend-entrypoint.sh
@@ -8,13 +8,13 @@ echo "Waiting for Postgres..."
 # Heroku mode
 if test -n ${DATABASE_URL}; then
   until psql "$DATABASE_URL" -c '\q'; do
-    echo >&2 "Postgres is unavailable - sleeping"
+    echo >&2 "Postgres is unavailable - sleeping (Heroku mode)"
     sleep 2
   done
 # Docker Compose mode
 else
   until PGPASSWORD=$POSTGRES_PASSWORD psql -h $DB_HOST -U postgres -c '\q'; do
-    echo >&2 "Postgres is unavailable - sleeping"
+    echo >&2 "Postgres is unavailable - sleeping (Docker Compose mode)"
     sleep 2
   done
 fi
