97270ba8d51b087d43f1e243330f955b1f8dcef7
Kevin Kaland <kevin@wizone.solutions>
2021-11-08 17:10:15 +0100

    Fix build errors and warnings.

--- bedes-common/models/uuid-generator/uuid-generator.ts
+++ bedes-common/models/uuid-generator/uuid-generator.ts
@@ -1,4 +1,4 @@
-import { v4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 /**
  * Wrapper class around the node uuid functionality.
@@ -8,6 +8,6 @@ export class UUIDGenerator {
      * Generates a "random" uuid.
      */
     protected generateUUID(): string {
-        return v4();
+        return uuidv4();
     }
 }
